f628088eb450a018a5e04732ab0761e2f52d5b61
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-04-11 16:45:51 +0900

    test : 더미데이터 추가

--- src/main/java/com/modagbul/BE/test/controller/TestController.java
+++ src/main/java/com/modagbul/BE/test/controller/TestController.java
@@ -8,11 +8,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class TestController {
     @GetMapping("test")
-    public ResponseEntity<t> testApi() {
-        return new ResponseEntity( new t("손현석", 26, "숭실"), HttpStatus.OK);
+    public ResponseEntity<List<t>> testApi() {
+        List<t> list = new ArrayList<>();
+        list.add(new t("손현석", 26, "숭실"));
+        list.add(new t("정승연",24,"숭실"));
+        list.add( new t("곽승엽",25,"숭실"));
+        return new ResponseEntity( list, HttpStatus.OK);
     }
 
 
