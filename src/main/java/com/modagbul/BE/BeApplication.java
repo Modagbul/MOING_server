f7464d96d4ce3c3b040bf63302882f1748cefae2
Minsu Kim <syt07100@gmail.com>
2023-04-28 13:42:26 +0900

    fix : application log

--- src/main/java/com/modagbul/BE/BeApplication.java
+++ src/main/java/com/modagbul/BE/BeApplication.java
@@ -8,6 +8,10 @@ public class BeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
+
+		// 메모리 사용량 출력
+		long heapSize = Runtime.getRuntime().totalMemory();
+		System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
 	}
 
 }
