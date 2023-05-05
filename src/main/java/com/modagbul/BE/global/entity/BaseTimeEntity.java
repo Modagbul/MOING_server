98a824fc921a53a7ef59cb88c8adb16c3d174ee9
Minsu Kim <syt07100@gmail.com>
2023-05-05 21:56:18 +0900

    fix: BaseTimeEntity convert 추가

--- src/main/java/com/modagbul/BE/global/entity/BaseTimeEntity.java
+++ src/main/java/com/modagbul/BE/global/entity/BaseTimeEntity.java
@@ -4,9 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateTimeConverter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -18,10 +20,15 @@ import java.time.LocalDateTime;
 public class BaseTimeEntity {
 
     @CreatedDate
-    @Column
+    @Column(updatable = false)
+    @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
+    @Column
+    @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime lastModifiedDate;
+
+
 }
 
