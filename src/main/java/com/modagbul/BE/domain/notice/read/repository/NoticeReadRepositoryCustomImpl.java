66a1f88afd08b26115f86c647553139b134a5ccd
Minsu Kim <syt07100@gmail.com>
2023-05-05 17:25:59 +0900

    fix: 디렉토리 변화에 따른 Querydsl import 수정

--- src/main/java/com/modagbul/BE/domain/notice/read/repository/NoticeReadRepositoryCustomImpl.java
+++ src/main/java/com/modagbul/BE/domain/notice/read/repository/NoticeReadRepositoryCustomImpl.java
@@ -5,7 +5,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import javax.persistence.EntityManager;
 import java.util.List;
 
-import static com.modagbul.BE.domain.notice_read.entity.QNoticeRead.noticeRead;
+import static com.modagbul.BE.domain.notice.read.entity.QNoticeRead.noticeRead;
 
 public class NoticeReadRepositoryCustomImpl implements NoticeReadRepositoryCustom{
     private final JPAQueryFactory queryFactory;
