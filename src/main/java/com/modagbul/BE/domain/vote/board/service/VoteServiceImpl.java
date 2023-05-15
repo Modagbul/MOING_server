059fcb1271c4487bfab7746fc5963a82294d96a4
Minsu Kim <syt07100@gmail.com>
2023-05-15 13:53:46 +0900

    feat: fcm 신규 투표 업로드 알림

--- src/main/java/com/modagbul/BE/domain/vote/board/service/VoteServiceImpl.java
+++ src/main/java/com/modagbul/BE/domain/vote/board/service/VoteServiceImpl.java
@@ -21,6 +21,8 @@ import com.modagbul.BE.domain.vote.content.user.enttiy.VoteContentUser;
 import com.modagbul.BE.domain.vote.content.user.repository.VoteContentUserRepository;
 import com.modagbul.BE.domain.vote.read.entity.VoteRead;
 import com.modagbul.BE.domain.vote.read.repository.VoteReadRepository;
+import com.modagbul.BE.fcm.dto.FcmDto;
+import com.modagbul.BE.fcm.service.FcmService;
 import com.modagbul.BE.global.config.security.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,11 +31,13 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
+import static com.modagbul.BE.fcm.constant.FcmConstant.NewUploadTitle.UPLOAD_VOTE_NEW_TITLE;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
 @Transactional
-public class VoteServiceImpl implements VoteService{
+public class VoteServiceImpl implements VoteService {
 
     private final VoteMapper voteMapper;
     private final VoteRepository voteRepository;
@@ -43,11 +47,12 @@ public class VoteServiceImpl implements VoteService{
     private final UserRepository userRepository;
     private final VoteContentUserRepository voteContentUserRepository;
     private final TeamService teamService;
+    private final FcmService fcmService;
 
     @Override
     public CreateVoteResponse createVote(Long teamId, CreateVoteRequest createVoteRequest) {
         //1. 투표  생성, 저장
-        Vote vote=voteMapper.toEntity(teamId, createVoteRequest);
+        Vote vote = voteMapper.toEntity(teamId, createVoteRequest);
         voteRepository.save(vote);
 
         //2. 투표 선택지 저장
@@ -55,13 +60,16 @@ public class VoteServiceImpl implements VoteService{
 
         //3. 투표-읽음 db 생성
         createVoteRead(teamId, vote);
+
+        //4. 투표 fcm 알림
+        sendNewUploadVoteAlarm(vote, teamId, SecurityUtils.getLoggedInUser().getUserId());
         return new CreateVoteResponse(vote.getVoteId());
     }
 
     @Override
     public void doVote(Long teamId, Long voteId, DoVoteRequest doVoteRequest) {
         //1. 유효성 체크
-        Vote vote=validateVote(teamId, voteId);
+        Vote vote = validateVote(teamId, voteId);
         //2. 투표 선택지 업데이트
         updateVoteContent(doVoteRequest, vote);
         //3. 읽음처리 업데이트
@@ -71,7 +79,7 @@ public class VoteServiceImpl implements VoteService{
     @Override
     public GetVoteDetailsResponse getVoteDetail(Long teamId, Long voteId) {
         //1. 유효성 체크
-        Vote vote=validateVote(teamId, voteId);
+        Vote vote = validateVote(teamId, voteId);
         //2. 읽음처리 업데이트
         updateVoteRead(vote);
         //3. 투표 조회
@@ -87,7 +95,7 @@ public class VoteServiceImpl implements VoteService{
 
     @Override
     public GetVoteAllResponse getVoteAll(Long teamId) {
-        return voteRepository.getVoteAllByTeamId(teamId,SecurityUtils.getLoggedInUser().getUserId());
+        return voteRepository.getVoteAllByTeamId(teamId, SecurityUtils.getLoggedInUser().getUserId());
     }
 
     @Override
@@ -97,24 +105,26 @@ public class VoteServiceImpl implements VoteService{
 
     /**
      * 투표가 종료되었는지 확인하는 메서드 (유효성 체크 메서드)
+     *
      * @param voteId
      * @return 투표가 종료되지 않으면 Vote 반환
      */
 
     @Override
-    public Vote validateVote(Long teamId, Long voteId){
-        Team team=teamService.validateTeam(teamId);
-        return this.voteRepository.findNotClosedByVoteId(voteId).orElseThrow(()->new NotFoundVoteIdException());
+    public Vote validateVote(Long teamId, Long voteId) {
+        Team team = teamService.validateTeam(teamId);
+        return this.voteRepository.findNotClosedByVoteId(voteId).orElseThrow(() -> new NotFoundVoteIdException());
     }
 
     /**
      * 투표 선택지를 저장하는 메서드
+     *
      * @param createVoteRequest
      */
-    private void createVoteContent(CreateVoteRequest createVoteRequest, Vote vote){
-        List<String> contents=createVoteRequest.getChoices();
-        contents.stream().forEach(content->{
-            VoteContent voteContent=new VoteContent();
+    private void createVoteContent(CreateVoteRequest createVoteRequest, Vote vote) {
+        List<String> contents = createVoteRequest.getChoices();
+        contents.stream().forEach(content -> {
+            VoteContent voteContent = new VoteContent();
             voteContent.setContent(content);
             voteContent.setVote(vote);
             voteContentRepository.save(voteContent);
@@ -123,18 +133,19 @@ public class VoteServiceImpl implements VoteService{
 
     /**
      * 투표할 때 투표 선택지를 업데이트하는 메서드 : 이때 해당 유저가 기존에 투표했던거는 삭제
+     *
      * @param doVoteRequest
      */
-    private void updateVoteContent(DoVoteRequest doVoteRequest, Vote vote){
+    private void updateVoteContent(DoVoteRequest doVoteRequest, Vote vote) {
         //해당 유저가 기존에 투표했던 거 삭제
         voteContentUserRepository.deleteAllByUser(SecurityUtils.getLoggedInUser());
         //투표한 사람 업데이트
-        List<String> contents=doVoteRequest.getChoices();
-        contents.stream().forEach(content->{
-            VoteContent voteContent=voteContentRepository.findByContentAndVote(content, vote).orElseThrow(()->new NotFoundVoteContentException());
-            VoteContentUser voteContentUser=new VoteContentUser();
+        List<String> contents = doVoteRequest.getChoices();
+        contents.stream().forEach(content -> {
+            VoteContent voteContent = voteContentRepository.findByContentAndVote(content, vote).orElseThrow(() -> new NotFoundVoteContentException());
+            VoteContentUser voteContentUser = new VoteContentUser();
             voteContentUser.setVoteContent(voteContent);
-            voteContentUser.setUser(userRepository.findById(SecurityUtils.getLoggedInUser().getUserId()).orElseThrow(()->new NotFoundEmailException()));
+            voteContentUser.setUser(userRepository.findById(SecurityUtils.getLoggedInUser().getUserId()).orElseThrow(() -> new NotFoundEmailException()));
             voteContentUser.setVote(vote);
             voteContentUserRepository.save(voteContentUser);
         });
@@ -143,13 +154,14 @@ public class VoteServiceImpl implements VoteService{
 
     /**
      * 투표 읽음 정보 디비를 생성하는 메소드 : 생성한 사람은 무조건 읽음 처리
+     *
      * @param teamId, vote
      */
-    private void createVoteRead(Long teamId, Vote vote){
+    private void createVoteRead(Long teamId, Vote vote) {
         //읽음 정보 데이터 생성
         List<TeamMember> teamMembers = teamMemberRepository.findByTeamId(teamId);
         teamMembers.stream().forEach(teamMember -> {
-            VoteRead voteRead=new VoteRead();
+            VoteRead voteRead = new VoteRead();
             voteRead.setVote(vote);
             voteRead.setUser(teamMember.getUser());
             voteReadRepository.save(voteRead);
@@ -161,25 +173,44 @@ public class VoteServiceImpl implements VoteService{
 
     /**
      * 지금 현재 유저를 읽음 처리하는 메서드
+     *
      * @param vote
      */
 
-    private void updateVoteRead(Vote vote){
-        VoteRead voteRead=voteReadRepository.findByUserAndVote(userRepository.findById
-                                (SecurityUtils.getLoggedInUser().getUserId()).orElseThrow(()->new NotFoundEmailException()),
+    private void updateVoteRead(Vote vote) {
+        VoteRead voteRead = voteReadRepository.findByUserAndVote(userRepository.findById
+                                (SecurityUtils.getLoggedInUser().getUserId()).orElseThrow(() -> new NotFoundEmailException()),
                         vote)
-                .orElseThrow(()-> new NotFoundVoteUserException());
+                .orElseThrow(() -> new NotFoundVoteUserException());
         voteRead.readVote();
     }
 
     /**
      * 투표를 작성한 유저인지 확인하는 메서드
+     *
      * @param user
      * @param vote
      */
 
-    private void validateUser(User user, Vote vote){
-        if(vote.getUser().getUserId()!=user.getUserId())
+    private void validateUser(User user, Vote vote) {
+        if (vote.getUser().getUserId() != user.getUserId())
             throw new NotNoticeWriterException();
+    }
+
+    /**
+     * Fcm 이용해서 알림 메시지 보내는 메서드
+     *
+     * @param userId
+     */
+    public void sendNewUploadVoteAlarm(Vote vote, Long teamId, Long userId) {
+        Team team = teamService.validateTeam(teamId);
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundEmailException());
+        //신규 업로드 알림이 true인지 확인
+        if (user.isNewUploadPush()) {
+            String title = team.getName() + " " + UPLOAD_VOTE_NEW_TITLE.getTitle();
+            String message = vote.getTitle();
+            FcmDto.ToSingleRequest toSingleRequest = new FcmDto.ToSingleRequest(user.getFcmToken(), title, message);
+            fcmService.sendSingleDevice(toSingleRequest);
+        }
     }
 }
