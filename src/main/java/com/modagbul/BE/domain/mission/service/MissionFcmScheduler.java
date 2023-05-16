5f94e38ef3ad079a90cbdf97aa5d0bce1d6f5c9c
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-05-16 18:41:59 +0900

    fix : schduler cron 표현식 수정

--- src/main/java/com/modagbul/BE/domain/mission/service/MissionFcmScheduler.java
+++ src/main/java/com/modagbul/BE/domain/mission/service/MissionFcmScheduler.java
@@ -23,7 +23,7 @@ public class MissionFcmScheduler {
 
 
 //    @Scheduled(cron = "0/30 * * * * *") // test를 위해 30초에 한번씩 실행하도록 함.
-    @Scheduled(cron = "0 6 * * *") // 매일 오전 6시에 이 메소드를 실행함.
+    @Scheduled(cron = "0 0 6 * * *") // 매일 오전 6시에 이 메소드를 실행함.
     public void checkDailyOneDayBefore() throws FirebaseMessagingException {
         // 지금 시간 기준, 하루 전 23:59:59 ~ 00:00:01 가 포함된 미션들을 가져옴.
 
