50b94a80f51fa810c0e2446248065326bc6eff38
정 승 연 <53565255+seungyeonnnnnni@users.noreply.github.com>
2023-08-02 04:50:36 +0900

    refactor : userMission 의존성 수정

--- src/main/java/com/modagbul/BE/domain/usermission/presentation/UserMissionController.java
+++ src/main/java/com/modagbul/BE/domain/usermission/presentation/UserMissionController.java
@@ -5,6 +5,8 @@ import com.modagbul.BE.domain.mission.main.application.service.MissionReadServic
 import com.modagbul.BE.domain.usermission.application.constant.Status;
 import com.modagbul.BE.domain.usermission.application.dto.UserMissionDetailDto;
 import com.modagbul.BE.domain.usermission.application.dto.UserMissionStatusDto;
+import com.modagbul.BE.domain.usermission.application.service.UserMissionCreateService;
+import com.modagbul.BE.domain.usermission.application.service.UserMissionReadService;
 import com.modagbul.BE.domain.usermission.application.service.UserMissionService;
 import com.modagbul.BE.global.dto.ResponseDto;
 import io.swagger.annotations.Api;
@@ -24,26 +26,27 @@ import static com.modagbul.BE.domain.mission.main.application.constant.MissionCo
 @RequestMapping("/api/v1/{teamId}/missions")
 public class UserMissionController {
 
-    private final UserMissionService userMissionService;
+    private final UserMissionCreateService userMissionCreateService;
+    private final UserMissionReadService userMissionReadService;
     private final MissionReadService missionReadService;
 
     @ApiOperation(value = "개인별 미션 제출", notes = "개인별 미션을 제출합니다.")
     @PostMapping("/{missionId}/submit")
     public ResponseEntity<ResponseDto<Status>> submitMission(@PathVariable Long teamId, @PathVariable Long missionId, @RequestBody String submitUrl){
-        return ResponseEntity.ok(ResponseDto.create(HttpStatus.OK.value(),SUBMIT_MISSION_SUCCESS.getMessage(),userMissionService.submitUserMission(teamId,missionId,submitUrl)));
+        return ResponseEntity.ok(ResponseDto.create(HttpStatus.OK.value(),SUBMIT_MISSION_SUCCESS.getMessage(),userMissionCreateService.submitUserMission(teamId,missionId,submitUrl)));
     }
 
     @ApiOperation(value = "개인별 미션 건너뛰기", notes = "개인별 미션을 건너뛰기합니다.")
     @PostMapping("/{missionId}/skip")
     public ResponseEntity<ResponseDto<Status>> skipMission(@PathVariable Long teamId, @PathVariable Long missionId, @RequestBody String skipReason){
-        return ResponseEntity.ok(ResponseDto.create(HttpStatus.OK.value(),SKIP_MISSION_SUCCESS.getMessage(),userMissionService.skipUserMission(teamId,missionId,skipReason)));
+        return ResponseEntity.ok(ResponseDto.create(HttpStatus.OK.value(),SKIP_MISSION_SUCCESS.getMessage(),userMissionCreateService.skipUserMission(teamId,missionId,skipReason)));
     }
 
 
     @ApiOperation(value = "개인별 미션 인증 현황", notes = "개인별 미션 인증 현황을 조회합니다..")
     @GetMapping("/{missionId}/status")
     public ResponseEntity<ResponseDto<UserMissionStatusDto>> statusMission(@PathVariable Long teamId, @PathVariable Long missionId){
-        return ResponseEntity.ok(ResponseDto.create(HttpStatus.OK.value(),STATUS_MISSION_SUCCESS.getMessage(),userMissionService.getUserMissionList(teamId,missionId)));
+        return ResponseEntity.ok(ResponseDto.create(HttpStatus.OK.value(),STATUS_MISSION_SUCCESS.getMessage(),userMissionReadService.getUserMissionList(teamId,missionId)));
     }
 
 
